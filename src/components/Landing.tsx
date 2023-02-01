485620ddf777482649cfa5ae2cb0832fe456056b
LucasHVicente <l.vicente9003@gmail.com>
2023-02-01 12:26:52 -0300

    add: added start fade  animation and text writer on title

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,18 +1,32 @@
+import { TypeAnimation } from 'react-type-animation';
 import { LinkButton } from './common/LinkButton';
 
 export const Landing = () => (
   <div className="flex w-full min-h-full lg:min-h-[80vh] items-center flex-col">
     <div className="section-div">
-      <h2 className="text-blue-700 text-xl lg:text-2xl">Olá, meu nome é</h2>
+      <h2 className="text-blue-700 text-xl lg:text-2xl">
+        <TypeAnimation
+          speed={50}
+          cursor={false}
+          sequence={['Olá, meu nome é']}
+        />
+      </h2>
       <h1 className="text-blue-500 font-semibold text-3xl lg:text-5xl">
-        {'<Lucas Vicente/>'}
+        <TypeAnimation cursor={false} sequence={[1000, '<Lucas Vicente/>']} />
       </h1>
       <h2 className="text-blue-700 text-xl lg:text-3xl">
-        e eu sou um desenvolvedor web.
+        <TypeAnimation
+          speed={50}
+          cursor={false}
+          className="typing-cursor"
+          sequence={[2000, 'e eu sou um desenvolvedor web.']}
+        />
       </h2>
-      <p>Sou desenvolvedor com foco em desenvolvimento front-end e mobile.</p>
+      <p className="start-animation">
+        Sou desenvolvedor com foco em desenvolvimento front-end e mobile.
+      </p>
     </div>
-    <div className="flex lg:w-[60%] justify-start gap-6 pt-4">
+    <div className="start-animation flex lg:w-[60%] justify-start gap-6 pt-4">
       <LinkButton label="Saiba mais" href="#about" />
       <LinkButton label="Entre em contato" href="#contact" />
     </div>
