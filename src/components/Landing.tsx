e39bc0bbd01a1e29c95b0e8670d06f4b38039841
LucasHVicente <l.vicente9003@gmail.com>
2023-02-01 11:30:11 -0300

    add: smooth scroll and base css changes

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,7 +1,7 @@
 import { LinkButton } from './common/LinkButton';
 
 export const Landing = () => (
-  <div className="flex w-full items-center flex-col">
+  <div className="flex w-full min-h-full lg:min-h-[80vh] items-center flex-col">
     <div className="section-div">
       <h2 className="text-blue-700 text-xl lg:text-2xl">Olá, meu nome é</h2>
       <h1 className="text-blue-500 font-semibold text-3xl lg:text-5xl">
@@ -13,6 +13,7 @@ export const Landing = () => (
       <p>Sou desenvolvedor com foco em desenvolvimento front-end e mobile.</p>
     </div>
     <div className="flex lg:w-[60%] justify-start gap-6 pt-4">
+      <LinkButton label="Saiba mais" href="#about" />
       <LinkButton label="Entre em contato" href="#contact" />
     </div>
   </div>
